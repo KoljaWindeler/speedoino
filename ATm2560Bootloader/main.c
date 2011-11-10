5c19564419d7c9179c70c6f4e20bb5f3ee2ab688
Kolja Windeler <KKoolljjaa@gmail.com>
2011-11-10 08:37:59 +0100

    restored original code

--- ATm2560Bootloader/main.c
+++ ATm2560Bootloader/main.c
@@ -92,13 +92,12 @@
 
 
 /* 20070707: hacked by David A. Mellis - after this many errors give up and launch application */
-#define MAX_ERROR_COUNT 15
-#define MAX_TIME_COUNT F_CPU>>5
+#define MAX_ERROR_COUNT 5
 
 /* set the UART baud rate */
 /* 20060803: hacked by DojoCorp */
 #ifndef BAUD_RATE
-#define BAUD_RATE   57600
+	#define BAUD_RATE   57600
 #endif
 
 
@@ -111,7 +110,7 @@
 
 /* monitor functions will only be compiled when using ATmega128, due to bootblock size constraints */
 #define MONITOR 1
-
+#define MAX_TIME_COUNT F_CPU >> 5
 
 
 /* define various device id's */
@@ -164,14 +163,14 @@ uint8_t error_count = 0;
 //******************************************************************************************
 void 	app_start()
 {
-	//*	May 23,	2010	<MLS> Modified app_start because it was not working on the 2560.
-	//*	Thanks to Peter Knight on the arduino.cc forum for this little trick
-	//*	http://www.arduino.cc/cgi-bin/yabb2/YaBB.pl?num=1271415071
-	asm volatile(
-			"clr	r30		\n\t"
-			"clr	r31		\n\t"
-			"ijmp	\n\t"
-	);
+//*	May 23,	2010	<MLS> Modified app_start because it was not working on the 2560.
+//*	Thanks to Peter Knight on the arduino.cc forum for this little trick
+//*	http://www.arduino.cc/cgi-bin/yabb2/YaBB.pl?num=1271415071
+asm volatile(
+		"clr	r30		\n\t"
+		"clr	r31		\n\t"
+		"ijmp	\n\t"
+		);
 }
 
 
@@ -196,8 +195,8 @@ int main(void)
 	asm volatile("nop\n\t");
 #endif
 
-	/* the mega1280 chip has four serial ports ... we could eventually use any of them, or not? */
-	/* however, we don't wanna confuse people, to avoid making a mess, we will stick to RXD0, TXD0 */
+/* the mega1280 chip has four serial ports ... we could eventually use any of them, or not? */
+/* however, we don't wanna confuse people, to avoid making a mess, we will stick to RXD0, TXD0 */
 
 
 	/* initialize UART(s) depending on CPU defined */
@@ -227,6 +226,7 @@ int main(void)
 
 	putch(0x14); /* here i am*/
 	putch(0x10); /* here i am*/
+
 
 
 	/* forever loop */
@@ -315,11 +315,11 @@ int main(void)
 		else if(ch=='Q')
 		{
 			nothing_response();
-#ifdef WATCHDOG_MODS
+	#ifdef WATCHDOG_MODS
 			// autoreset via watchdog (sneaky!)
 			WDTCSR	=	_BV(WDE);
 			while (1); // 16 ms
-#endif
+	#endif
 		}
 
 
@@ -407,81 +407,81 @@ int main(void)
 					cli();					//Disable interrupts, just to be sure
 					while(bit_is_set(EECR,EEPE));			//Wait for previous EEPROM writes to complete
 					asm volatile(
-							"clr	r17		\n\t"				//page_word_count
-							"lds	r30,address	\n\t"			//Address of FLASH location (in bytes)
-							"lds	r31,address+1	\n\t"
-							"ldi	r28,lo8(buff)	\n\t"		//Start of buffer array in RAM
-							"ldi	r29,hi8(buff)	\n\t"
-							"lds	r24,length	\n\t"			//Length of data to be written (in bytes)
-							"lds	r25,length+1	\n\t"
-							"length_loop:		\n\t"					//Main loop, repeat for number of words in block
-							"cpi	r17,0x00	\n\t"			//If page_word_count=0 then erase page
-							"brne	no_page_erase	\n\t"
-							"wait_spm1:		\n\t"
-							"lds	r16,%0		\n\t"			//Wait for previous spm to complete
-							"andi	r16,1           \n\t"
-							"cpi	r16,1           \n\t"
-							"breq	wait_spm1       \n\t"
-							"ldi	r16,0x03	\n\t"			//Erase page pointed to by Z
-							"sts	%0,r16		\n\t"
-							"spm			\n\t"
-							"wait_spm2:		\n\t"
-							"lds	r16,%0		\n\t"			//Wait for previous spm to complete
-							"andi	r16,1           \n\t"
-							"cpi	r16,1           \n\t"
-							"breq	wait_spm2       \n\t"
+						 "clr	r17		\n\t"				//page_word_count
+						 "lds	r30,address	\n\t"			//Address of FLASH location (in bytes)
+						 "lds	r31,address+1	\n\t"
+						 "ldi	r28,lo8(buff)	\n\t"		//Start of buffer array in RAM
+						 "ldi	r29,hi8(buff)	\n\t"
+						 "lds	r24,length	\n\t"			//Length of data to be written (in bytes)
+						 "lds	r25,length+1	\n\t"
+				"length_loop:		\n\t"					//Main loop, repeat for number of words in block
+						 "cpi	r17,0x00	\n\t"			//If page_word_count=0 then erase page
+						 "brne	no_page_erase	\n\t"
+				"wait_spm1:		\n\t"
+						 "lds	r16,%0		\n\t"			//Wait for previous spm to complete
+						 "andi	r16,1           \n\t"
+						 "cpi	r16,1           \n\t"
+						 "breq	wait_spm1       \n\t"
+						 "ldi	r16,0x03	\n\t"			//Erase page pointed to by Z
+						 "sts	%0,r16		\n\t"
+						 "spm			\n\t"
+				"wait_spm2:		\n\t"
+						 "lds	r16,%0		\n\t"			//Wait for previous spm to complete
+						 "andi	r16,1           \n\t"
+						 "cpi	r16,1           \n\t"
+						 "breq	wait_spm2       \n\t"
 
-							"ldi	r16,0x11	\n\t"			//Re-enable RWW section
-							"sts	%0,r16		\n\t"
-							"spm			\n\t"
-							"no_page_erase:		\n\t"
-							"ld	r0,Y+		\n\t"			//Write 2 bytes into page buffer
-							"ld	r1,Y+		\n\t"
+						 "ldi	r16,0x11	\n\t"			//Re-enable RWW section
+						 "sts	%0,r16		\n\t"
+						 "spm			\n\t"
+				"no_page_erase:		\n\t"
+						 "ld	r0,Y+		\n\t"			//Write 2 bytes into page buffer
+						 "ld	r1,Y+		\n\t"
 
-							"wait_spm3:		\n\t"
-							"lds	r16,%0		\n\t"			//Wait for previous spm to complete
-							"andi	r16,1       \n\t"
-							"cpi	r16,1       \n\t"
-							"breq	wait_spm3   \n\t"
-							"ldi	r16,0x01	\n\t"			//Load r0,r1 into FLASH page buffer
-							"sts	%0,r16		\n\t"
-							"spm				\n\t"
+				"wait_spm3:		\n\t"
+						 "lds	r16,%0		\n\t"			//Wait for previous spm to complete
+						 "andi	r16,1       \n\t"
+						 "cpi	r16,1       \n\t"
+						 "breq	wait_spm3   \n\t"
+						 "ldi	r16,0x01	\n\t"			//Load r0,r1 into FLASH page buffer
+						 "sts	%0,r16		\n\t"
+						 "spm				\n\t"
 
-							"inc	r17			\n\t"			//page_word_count++
-							"cpi r17,%1	    \n\t"
-							"brlo	same_page	\n\t"			//Still same page in FLASH
-							"write_page:				\n\t"
-							"clr	r17			\n\t"			//New page, write current one first
-							"wait_spm4:		\n\t"
-							"lds	r16,%0		\n\t"			//Wait for previous spm to complete
-							"andi	r16,1       \n\t"
-							"cpi	r16,1       \n\t"
-							"breq	wait_spm4   \n\t"
-							"ldi	r16,0x05	\n\t"			//Write page pointed to by Z
-							"sts	%0,r16		\n\t"
-							"spm			\n\t"
-							"wait_spm5:		\n\t"
-							"lds	r16,%0		\n\t"			//Wait for previous spm to complete
-							"andi	r16,1           \n\t"
-							"cpi	r16,1           \n\t"
-							"breq	wait_spm5       \n\t"
-							"ldi	r16,0x11	\n\t"			//Re-enable RWW section
-							"sts	%0,r16		\n\t"
-							"spm			\n\t"
-							"same_page:		\n\t"
-							"adiw	r30,2		\n\t"			//Next word in FLASH
-							"sbiw	r24,2		\n\t"			//length-2
-							"breq	final_write	\n\t"			//Finished
-							"rjmp	length_loop	\n\t"
-							"final_write:		\n\t"
-							"cpi	r17,0		\n\t"
-							"breq	block_done	\n\t"
-							"adiw	r24,2		\n\t"			//length+2, fool above check on length after short page write
-							"rjmp	write_page	\n\t"
-							"block_done:		\n\t"
-							"clr	__zero_reg__	\n\t"		//restore zero register
-							: "=m" (SPMCSR) : "M" (PAGE_SIZE) : "r0","r16","r17","r24","r25","r28","r29","r30","r31"
-					);
+						 "inc	r17			\n\t"			//page_word_count++
+						 "cpi r17,%1	    \n\t"
+						 "brlo	same_page	\n\t"			//Still same page in FLASH
+				"write_page:				\n\t"
+						 "clr	r17			\n\t"			//New page, write current one first
+				"wait_spm4:		\n\t"
+						 "lds	r16,%0		\n\t"			//Wait for previous spm to complete
+						 "andi	r16,1       \n\t"
+						 "cpi	r16,1       \n\t"
+						 "breq	wait_spm4   \n\t"
+						 "ldi	r16,0x05	\n\t"			//Write page pointed to by Z
+						 "sts	%0,r16		\n\t"
+						 "spm			\n\t"
+				"wait_spm5:		\n\t"
+						 "lds	r16,%0		\n\t"			//Wait for previous spm to complete
+						 "andi	r16,1           \n\t"
+						 "cpi	r16,1           \n\t"
+						 "breq	wait_spm5       \n\t"
+						 "ldi	r16,0x11	\n\t"			//Re-enable RWW section
+						 "sts	%0,r16		\n\t"
+						 "spm			\n\t"
+				"same_page:		\n\t"
+						 "adiw	r30,2		\n\t"			//Next word in FLASH
+						 "sbiw	r24,2		\n\t"			//length-2
+						 "breq	final_write	\n\t"			//Finished
+						 "rjmp	length_loop	\n\t"
+				"final_write:		\n\t"
+						 "cpi	r17,0		\n\t"
+						 "breq	block_done	\n\t"
+						 "adiw	r24,2		\n\t"			//length+2, fool above check on length after short page write
+						 "rjmp	write_page	\n\t"
+				"block_done:		\n\t"
+						 "clr	__zero_reg__	\n\t"		//restore zero register
+						 : "=m" (SPMCSR) : "M" (PAGE_SIZE) : "r0","r16","r17","r24","r25","r28","r29","r30","r31"
+						 );
 					/* Should really add a wait for RWW section to be enabled, don't actually need it since we never */
 					/* exit the bootloader without a power cycle anyhow */
 				}
@@ -556,7 +556,7 @@ int main(void)
 		}
 
 
-#if defined MONITOR
+	#if defined MONITOR
 
 		/* here come the extended monitor commands by Erik Lins */
 
@@ -657,11 +657,11 @@ int main(void)
 		}
 		/* end of monitor */
 #endif
-		else if (++error_count == MAX_ERROR_COUNT)
-		{
-			app_start();
-		}
-	} /* end of forever loop */
+	else if (++error_count == MAX_ERROR_COUNT)
+	{
+		app_start();
+	}
+} /* end of forever loop */
 
 }
 
