e2618b25192895801cc298dca76d561364b2cfb3
Kolja Windeler <KKoolljjaa@gmail.com>
2012-08-31 20:55:25 +0200

    Changed to stk500v2 bootloader in eclipse

--- ATm328_Firmware/Release/subdir.mk
+++ ATm328_Firmware/Release/subdir.mk
@@ -29,7 +29,7 @@ C_DEPS += \
 %.o: ../%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: AVR Compiler'
-	avr-gcc -Wall -Os -fpack-struct -fshort-enums -std=gnu99 -funsigned-char -funsigned-bitfields -mmcu=atmega328p -DF_CPU=8000000UL -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -c -o "$@" "$<"
+	avr-gcc -I"/home/jkw/Store/17 - Speedmaster/ATm328_Firmware" -I/usr/lib/avr/include/ -Wall -Werror -Os -fpack-struct -fshort-enums -std=gnu99 -funsigned-char -funsigned-bitfields -mmcu=atmega328p -DF_CPU=8000000UL -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -c -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
