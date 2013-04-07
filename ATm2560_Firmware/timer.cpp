4862778e67d3c4be1d8ed17772fa24cca9b645e2
JKW <KKoolljjaa@gmail.com>
2013-04-07 14:18:01 +0200

    Timer polls Lights

--- ATm2560_Firmware/timer.cpp
+++ ATm2560_Firmware/timer.cpp
@@ -75,6 +75,11 @@ void speedo_timer::every_sec(configuration* pConfig) {
 void speedo_timer::every_qsec() {
 	if((millis()-every_qsecond_timer)>=250){
 		every_qsecond_timer=millis();
+		// polling  sensors
+		if(pSensors->last_int_state!=(CAN_INTERRUPT_PIN_PORT_V7 & 0x07)){
+			pSensors->last_int_state=(CAN_INTERRUPT_PIN_PORT_V7 & 0x07);
+			pSensors->check_inputs();
+		}
 		// see if its a clock startup or a regular startup
 		if(pSpeedo->regular_startup){
 			// TODO: warum so häufig?
